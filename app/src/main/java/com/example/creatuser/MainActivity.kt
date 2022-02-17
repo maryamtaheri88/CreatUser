b6398eba1e9d4b182230e5cd8aaaadb32e198d81
MaryamTaheri <mary.irani@gmail.com>
2022-02-18 00:58:23 +0330

    try to write info hiden

--- app/src/main/java/com/example/creatuser/MainActivity.kt
+++ app/src/main/java/com/example/creatuser/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
     val emailKey ="emailKey"
     val passwordKey ="passwordKey"
     val repasswordKey ="repasswordKey"
-    val genderKey ="genderKey"
+    val genderfKey ="genderKey"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,23 +35,40 @@ class MainActivity : AppCompatActivity() {
             var username = binding.edituserName.text.toString()
             var email = binding.editemailAddress.text.toString()
             var password = binding.editPassword.text.toString()
-            var repasswprd = binding.buttonRegister.text.toString()
-            var gender = binding.radioGroup.checkedRadioButtonId
+            var repassword = binding.buttonRegister.text.toString()
+            var genderf = binding.radioButtonFemale.isClickable
 
             var editor:SharedPreferences.Editor = prefs.edit()  //barai kar ba dadeha dar shareperferencese bayad yek editor besazim
             editor.putString(fullNameKey,fullName)   //barai ezafe kardane dade  dar shareperfernces az dastor put estefade mikonim m noe dadei ke mikhahim estefade konim ro bayad hatman moshakhas konim.
             editor.putString(userNameKey,username)  // agar var ma az noe Int bood: editor.putLong(nationalCodekey,nationalode.toLong())
             editor.putString(emailKey,email)
             editor.putString(passwordKey,password)
-            editor.putString(repasswordKey,repasswprd)
-            editor.putBoolean(genderKey,true)
+            editor.putString(repasswordKey,repassword)
+            editor.putBoolean(genderfKey,genderf)
             editor.apply() // taghirat ra taiid mikonad
 
             Toast.makeText(this,"Data saved.",Toast.LENGTH_SHORT).show()
        }
-        binding.buttonShowInfo.setOnClickListener {
-            var data ="FullName:${prefs.getString(fullNameKey,null)}\n UserName:${prefs.getString(userNameKey,null)}\n Email:${prefs.getString(emailKey,null)}"
-          Toast.makeText(this,data,Toast.LENGTH_LONG).show()
+
+        binding.buttonShowInfo.setOnClickListener{
+             binding.fullName.text = prefs.getString(fullNameKey, " name???")
+             binding.userName.text = prefs.getString(userNameKey, " username???")
+             binding.email.text = prefs.getString(emailKey, " email???")
+             binding.password.text = prefs.getString(passwordKey, "password???")
+             binding.gendar.text = prefs.getString(genderfKey, " gender???")
         }
+
+        binding.buttonRemoveInfo.setOnClickListener {
+
+        }
+
+
+//        binding.buttonShowInfo.setOnClickListener {
+//        var data ="FullName:${prefs.getString(fullNameKey,null)}\n UserName:${prefs.getString(userNameKey,null)}\n " +
+//                "Email:${prefs.getString(emailKey,null)}"
+//           Toast.makeText(this,data,Toast.LENGTH_LONG).show()
+//        }
+
+
     }
 }
--- app/src/main/java/com/example/creatuser/userModel.kt
+++ app/src/main/java/com/example/creatuser/userModel.kt
@@ -3,6 +3,4 @@ package com.example.creatuser
 class userModel (
      var userName:String,
      var password :String
-
-
  )
